b247c8eb8603bd26ae25091e52b2a899a4801c0d
Aditya Thummar <87354271+AdityaThummar@users.noreply.github.com>
2025-02-02 09:09:33 +0000

    uploaded dummy code

--- index.js
+++ index.js
@@ -14,24 +14,24 @@ app.get('/get-audio-url', async (req, res) => {
 
   try {
     // Validate the YouTube URL
-    if (!ytdl.validateURL(videoUrl)) {
-      return res.status(400).json({ error: 'Invalid YouTube URL.' });
-    }
+    // if (!ytdl.validateURL(videoUrl)) {
+    //   return res.status(400).json({ error: 'Invalid YouTube URL.' });
+    // }
 
-    // Get video info
-    const info = await ytdl.getInfo(videoUrl, {lang:"en"});
-    console.log("infoinfo", info)
+    // // Get video info
+    // const info = await ytdl.getInfo(videoUrl, {lang:"en"});
+    // console.log("infoinfo", info)
 
-    // Filter formats to get audio-only streams
-    const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+    // // Filter formats to get audio-only streams
+    // const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
 
-    if (audioFormats.length === 0) {
-      return res.status(404).json({ error: 'No audio formats found for this video.' });
-    }
+    // if (audioFormats.length === 0) {
+    //   return res.status(404).json({ error: 'No audio formats found for this video.' });
+    // }
 
-    // Select the first audio format (you can customize this logic)
-    const audioUrl = audioFormats[0].url;
-    // const audioUrl = ''
+    // // Select the first audio format (you can customize this logic)
+    // const audioUrl = audioFormats[0].url;
+    const audioUrl = ''
 
     // Return the audio URL
     res.json({ audioUrl });
